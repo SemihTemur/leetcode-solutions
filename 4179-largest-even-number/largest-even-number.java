3abb15a5ed656a14dac62e2ce98040f24c216374
Semih Temur <130358025+SemihTemur@users.noreply.github.com>
2026-02-07 19:55:22 +0300

    Time: 1 ms (59.56%) | Memory: 44.5 MB (5.91%) - LeetSync

--- 4179-largest-even-number/largest-even-number.java
+++ 4179-largest-even-number/largest-even-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public String largestEven(String s) {
-        if(s.length() == 1){
-            if(s.equals("2"))
-                return s;
-            else
-                return "";
-        }
+        // if(s.length() == 1){
+        //     if(s.equals("2"))
+        //         return s;
+        //     else
+        //         return "";
+        // }
 
         char[] largestEvenArray = s.toCharArray();
         int i = largestEvenArray.length - 1;
